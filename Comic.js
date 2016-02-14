f1f02780dbb9ebdc5da7f489aa9d70835a857c63
Brian Bauman <magus.bauman@gmail.com>
2016-02-14 02:40:25 -0800

    Working: Generating Comic from raw data.

--- Comic.js
+++ Comic.js
@@ -6,15 +6,22 @@
  * comicRawData = A complete "comics-item" nodeSet
  */
 function Comic(comicRawData) {
-    var progressLabel = comicRawData.getElementsByClassName('progress-label')[0].textContent.split('/');
-    var title = comicRawData.getElementsByClassName('comics-item-image')[0].firstChild.textContent;
-    var url = comicRawData.getElementsByClassName('comics-item-read')[0].getAttribute('href');
+}
 
-    this.baseURL = url.substring(0, url.lastIndexOf('/') + 1);
-    this.nextComic = parseInt(progressLabel[0]) + 1;
-    this.pages = [];
-    this.title = title;
-    this.unread = progressLabel[1] - progressLabel[0];
+Comic.fromRaw = function (rawData) {
+    var comic = new Comic();
+
+    var progressLabel = rawData.getElementsByClassName('progress-label')[0].textContent.split('/');
+    var title = rawData.getElementsByClassName('comics-item-image')[0].firstChild.textContent;
+    var url = rawData.getElementsByClassName('comics-item-read')[0].getAttribute('href');
+
+    comic.baseURL = url.substring(0, url.lastIndexOf('/') + 1);
+    comic.nextComic = parseInt(progressLabel[0]) + 1;
+    comic.pages = [];
+    comic.title = title;
+    comic.unread = progressLabel[1] - progressLabel[0];
+
+    return comic;
 }
 
 Comic.prototype.markPage = function (page) {
